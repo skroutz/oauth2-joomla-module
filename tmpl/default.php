1145880a5939fd112e08ed4c5846ccb0697c1053
Fotos Georgiadis <fotos@skroutz.gr>
2012-04-25 12:20:28 +0300

    Added en-GB and el-GR localizations

--- tmpl/default.php
+++ tmpl/default.php
@@ -13,11 +13,11 @@ defined('_JEXEC') or die('Restricted access'); ?>
 </form>
 <?php else : ?>
 <form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" name="login" id="form-login" >
-	<?php echo $params->get('pretext'); ?>
+	<?php echo JText::_($params->get('pretext')); ?>
 	<fieldset class="input">
 		<input type="submit" name="Submit" class="button" value="<?php echo JText::_('MOD_SKROUTZ_EASY_BUTTON_LOGIN') ?>" />
 	</fieldset>
-	<?php echo $params->get('posttext'); ?>
+	<?php echo JText::_($params->get('posttext')); ?>
 
 	<input type="hidden" name="option" value="com_skroutzeasy" />
 	<input type="hidden" name="task" value="login" />
