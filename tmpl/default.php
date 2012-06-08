11c325252056a09d9928c5db6ee3341deafc1a96
Fotos Georgiadis <fotos@skroutz.gr>
2012-06-08 16:28:48 +0300

    Added logout button localization

--- tmpl/default.php
+++ tmpl/default.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
 <?php if($type == 'logout') : ?>
 <form action="index.php" method="post" name="login" id="form-login">
 	<div align="center">
-		<input type="submit" name="Submit" class="button" value="<?php echo JText::_('BUTTON_LOGOUT'); ?>" />
+		<input type="submit" name="Submit" class="button" value="<?php echo JText::_('MOD_SKROUTZ_EASY_BUTTON_LOGOUT'); ?>" />
 	</div>
 
 	<input type="hidden" name="option" value="com_user" />
